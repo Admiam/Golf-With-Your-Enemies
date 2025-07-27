9a228079e4141af0f0daa921ae4ca34725f9b544
Admiam <admiam35@seznam.cz>
2025-07-27 12:24:57 +0200

    Initial Commit

---
+++ GolfWithYourEnemies/GolfWithYourEnemiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GolfWithYourEnemiesApp.swift
+//  GolfWithYourEnemies
+//
+//  Created by Adam Míka on 27.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct GolfWithYourEnemiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GolfWithYourEnemiesTests/GolfWithYourEnemiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  GolfWithYourEnemiesTests.swift
+//  GolfWithYourEnemiesTests
+//
+//  Created by Adam Míka on 27.07.2025.
+//
+
+import Testing
+@testable import GolfWithYourEnemies
+
+struct GolfWithYourEnemiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
